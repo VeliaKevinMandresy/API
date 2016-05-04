695f202114f836c43fcef8b019192432b9dba9d8
VELIA Kevin Mandresy <velia_k@etna-alternance.net>
2016-05-04 21:12:24 +0200

    Ajout de commentaire

--- sendIMNotification.php
+++ sendIMNotification.php
@@ -18,16 +18,14 @@ function login($password, $username)
     curl_setopt($ch, CURLOPT_COOKIE, "cookiename=0");
     curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //    curl_setopt($ch, CURLOPT_REFERER, $_SERVER['REQUEST_URI']);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-    
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $postinfo);
     curl_exec($ch);
     
-    // Http status
+    // Http status ( Check error )
     $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     //var_dump(curl_setopt($ch, CURLOPT_URL, $url));
 
@@ -36,7 +34,8 @@ function login($password, $username)
     
     $html = curl_exec($ch);
     curl_close($ch);
-    
+
+    //Display value of $http_status
      if ($http_status == 200)
          echo '[ '. $http_status .' ]' . " Vous avez reussi a vous connecter au site Rocket Chat\n";
      else
