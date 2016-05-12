87059da4c2d8e6fccc80b4bfed0698090e4ac4f7
VELIA Kevin Mandresy <velia_k@etna-alternance.net>
2016-05-13 00:00:13 +0200

    Commit final

--- Dynamique/sendIMNotification.php
+++ Dynamique/sendIMNotification.php
@@ -7,14 +7,14 @@ $url = "https://demo.rocket.chat/"; // URL de la demo Rocket Chat
 //$idRooms = "GENERAL"; // Room par default
 $idRooms = "whfpPjE2okzgBHLv5"; // Nom de Room "Anime"
 $message = "Bonjour, je suis kevin un ami a TOTO et j'adore les animes";
-
+//$message = "Goodnight everyone"; //Message test
 
 $API = new API_Rest(); //Class API_Rest fonctions.php
-/* $login = $API->login($Username, $Password, $url);
-   $joinRoom = $API->join($idRooms);
-   $sendMsg = $API->sendMessage($idRooms, $message); */
+$login = $API->login($Username, $Password, $url);
+$joinRoom = $API->join($idRooms);
+$sendMsg = $API->sendMessage($idRooms, $message);
+$listRooms = $API->publicRooms();
 
-$listRooms = $API->publicRooms(); 
 // Affichage
 print_r($listRooms);
 ?>
