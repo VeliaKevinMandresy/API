6707d2da563e43ae5e7889ba06f84419cac76b42
VELIA Kevin Mandresy <velia_k@etna-alternance.net>
2016-05-12 22:38:00 +0200

    Modification variable joinMessage -> joinRoom

--- Dynamique/sendIMNotification.php
+++ Dynamique/sendIMNotification.php
@@ -1,19 +1,21 @@
 <?php
-require_once('test.php');//Ajout du fichier contenant les fonction demander
+require_once('fonctions.php');//Ajout du fichier contenant les fonctions demander
 
 $Username = "kevin.mandresy.velia"; // Mon nom d'utilisateur pour la connexion
 $Password = "Mandresy95"; // Mon mot de passe
 $url = "https://demo.rocket.chat/"; // URL de la demo Rocket Chat
 $idRooms = "GENERAL"; // Room par default
-$idRooms = "007"; // Room Test
+$idRooms = ""; // Room Test
 
 $API = new API_Rest(); //Class API_Rest fonctions.php
 $login = $API->login($Username, $Password, $url);
 $publicRooms = $API->publicRooms(); 
-$joinMessage = $API->join($idRooms);
+$joinRoom = $API->join($idRooms);
 
 // Affichage
 print_r($login);
 print_r("\n");
-print_r($joinMessage);
+print_r($joinRoom);
+print_r("\n");
+print_r($sendMsg);
 ?>
