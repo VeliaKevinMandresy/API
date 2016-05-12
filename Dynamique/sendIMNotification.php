184bd280a3bf8ff08a50db89c3dd0c942b24d10c
VELIA Kevin Mandresy <velia_k@etna-alternance.net>
2016-05-12 22:23:35 +0200

    Ajout affichage message pour tester si fonction join est fonctionnelle

--- Dynamique/sendIMNotification.php
+++ Dynamique/sendIMNotification.php
@@ -4,12 +4,16 @@ require_once('test.php');//Ajout du fichier contenant les fonction demander
 $Username = "kevin.mandresy.velia"; // Mon nom d'utilisateur pour la connexion
 $Password = "Mandresy95"; // Mon mot de passe
 $url = "https://demo.rocket.chat/"; // URL de la demo Rocket Chat
+$idRooms = "GENERAL"; // Room par default
+$idRooms = "007"; // Room Test
 
 $API = new API_Rest(); //Class API_Rest fonctions.php
 $login = $API->login($Username, $Password, $url);
 $publicRooms = $API->publicRooms(); 
+$joinMessage = $API->join($idRooms);
 
 // Affichage
 print_r($login);
 print_r("\n");
+print_r($joinMessage);
 ?>
